e9183187b20fb913ff63c522632edb5489ee7c25
baileysweeta2C <baileysweetxFY@crazespaces.pw>
2014-10-11 17:50:02 -0500

    1

---
+++ PLX/MainTitleList-2.pl
@@ -0,0 +1,57 @@
+version=1
+title=themovielist2013
+
+
+
+# 9999998
+type=playlist
+name=Movies
+thumb=https://github.com/baileysweeta2C/Repository/raw/master/Art/Movies1.png
+date=2014-01-12
+URL=https://github.com/baileysweeta2C/Repository/raw/master/PLX/Featured.plx
+player=default
+rating=-1.00
+
+
+# 9999999
+type=playlist
+name=Kids Movies
+thumb=https://github.com/baileysweeta2C/Repository/raw/master/Art/MoviesKids.png
+date=2014-01-12
+URL=https://github.com/baileysweeta2C/Repository/raw/master/PLX/Kids.plx
+player=default
+rating=-1.00
+
+# 9999997
+type=playlist
+name=Bill Nye
+thumb=http://ia.media-imdb.com/images/M/MV5BMTI0NjcwNTkxOV5BMl5BanBnXkFtZTcwNjU5NjMyMQ@@._V1_SX214_AL_.jpg
+date=2014-01-12
+URL=https://github.com/baileysweeta2C/Repository/raw/master/PLX/BillNye.plx
+player=default
+rating=-1.00
+
+# 9999997
+type=playlist
+name=TV
+thumb=https://github.com/baileysweeta2C/Repository/raw/master/Art/livetv.png
+date=2014-01-12
+URL=http://www.navixtreme.com/cgi-bin/boseman/Scrapers/ct_freetv?q=getupdated
+player=default
+rating=-1.00
+
+
+# 9999994
+type=playlist
+name=Main Event
+thumb=https://raw.github.com/Willieret54/Femoter/master/Art/Sports-Logo.png
+date=2014-01-12
+URL=https://raw.github.com/Willieret54/Femoter/master/PLX/Sports.plx
+player=default
+rating=-1.00
+
+
+
+
+
+# cached
